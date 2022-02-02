40263b7a14e282b69e0110c31f4757f1d7fd519c
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-02-02 05:48:32 -0300

    User input
    Pego a entrada do usuário e seleciono as cartas aleatoriamente a partir
    dela em função das imagens disponíveis.

--- source/source.js
+++ source/source.js
@@ -1,8 +1,12 @@
+const comparador = () => Math.random() - 0.5; 
+
 const getNumberOfCards = () => {
     let numberOfCards = parseInt(prompt("Com quantas cartas deseja jogar?\n"
                                         +"Selecione um número par entre 4 e 14,"
                                         +" inclusives."));
-    while(numberOfCards > 14 || numberOfCards < 4 || numberOfCards%2 === 1) {
+    while(
+        numberOfCards > 14 || numberOfCards < 4 
+        || numberOfCards%2 === 1 || isNaN(numberOfCards)) {
         numberOfCards = parseInt(prompt("Insira um número válido.\n"
                                         +"Com quantas cartas deseja jogar?\n"
                                         +"Selecione um número par entre 4 e 14,"
@@ -11,14 +15,47 @@ const getNumberOfCards = () => {
     return numberOfCards
 }
 
-const setCards = (numberOfCards) => {
-    ul = document.getElementById("cards");
-    ul.innerHTML = '';
-    for(let i = 0; i < numberOfCards; i++) {
-        ul.innerHTML += `
-        <li class="card"></li>
-        `;
-    }
+const scrambleImages = (imageList) => {
+    const result = imageList;
+    result.sort(comparador);
+    return result
 }
 
-setCards(getNumberOfCards())
+const getSliceOfImages = (imageList, N) => imageList.slice(0,N);
+
+const extendImageList = (imageList) => imageList.concat(imageList);
+
+const setCards = (imageList) => {
+    ul = document.getElementById("cards");
+    ul.innerHTML = '';
+    imageList.forEach(imageURL => {
+        ul.innerHTML += `
+        <li class="card">
+            <img class="front-face" src="${imageURL}" alt="FRENTE">
+        </li>
+        `;
+    });
+    return null
+}
+
+parrotsImages = [
+    "../assets/img/bobrossparrot.gif",
+    "../assets/img/explodyparrot.gif",
+    "../assets/img/fiestaparrot.gif",
+    "../assets/img/metalparrot.gif",
+    "../assets/img/revertitparrot.gif",
+    "../assets/img/tripletsparrot.gif",
+    "../assets/img/unicornparrot.gif",
+];
+
+setCards(
+    scrambleImages(
+        extendImageList(
+            getSliceOfImages(
+                scrambleImages(parrotsImages),getNumberOfCards()/2
+            )
+        )
+    )
+);
+
+
